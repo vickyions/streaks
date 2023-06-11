4d965af5dd26fedbd1329644854c7f87bd4a1a1a
Vicky Soni <murlimanoharsoni4@gmail.com>
2023-06-11 21:40:18 +0530

    added responsive grid, month and year name

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # streaks
-simple barebone calendar streak tracker  to mark only one habit, or maybe just a simple calendar generator
+
+simple barebone calendar streak tracker to mark only one habit, or maybe just a simple calendar generator
 
 # todo
 
-- add styling
-    - better griding for months inside year
-    - dark mode/light mode
-- add month name
-- also add year name
-- add shifitng to different years
-- add localstorage
+- [X] add styling
+    - [X] better griding for months inside year
+    - [ ] dark mode/light mode
+- [X] add month name
+- [X] also add year name
+- [ ] add shifitng to different years
+- [ ] add localstorage
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fake = {
   2023: [
     [
       1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0,0
+      0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
     ],
     [],
     [],
@@ -23,46 +23,51 @@ const monthTemplate = document.getElementById("month-template");
 const dayTemplate = document.getElementById("day-template");
 const body = document.querySelector("body");
 
-
 //reads from fake and generate
-for(let y in fake) {
-    // debugger
-    const yClone = yearTemplate.content.cloneNode(true);
-    const ySection = yClone.querySelector('.year');
-    const Y = fake[y]?.length === 12 ? fake[y] : Array(12).fill([]);
+for (let y in fake) {
+  // debugger
+  const yClone = yearTemplate.content.cloneNode(true);
+  const ySection = yClone.querySelector(".year");
+  const yName = yClone.querySelector(".year_name");
+  yName.textContent = y;
+  const Y = fake[y]?.length === 12 ? fake[y] : Array(12).fill([]);
 
-    for (let m = 0; m < 12; m++) {
-        const firstDayObj = new Date(y, m, 1);
-        const dayOfWeek = firstDayObj.getDay(); //0 - 6
-        const monthObj = new Date(y, m + 1, 0); //gives last date
-        const noOfDays = monthObj.getDate();
+  for (let m = 0; m < 12; m++) {
+    const firstDayObj = new Date(y, m, 1);
+    const dayOfWeek = firstDayObj.getDay(); //0 - 6
+    const monthObj = new Date(y, m + 1, 0); //gives last date
+    const noOfDays = monthObj.getDate();
 
-        const mClone = monthTemplate.content.cloneNode(true);
-        const mArticle = mClone.querySelector('.month');
-        const M = Y[m]?.length === 37 ? Y[m] : Array(37).fill(0);
+    const mClone = monthTemplate.content.cloneNode(true);
+    const mArticle = mClone.querySelector(".month");
+    const mName = mClone.querySelector(".month_name");
+    mName.textContent = new Intl.DateTimeFormat("en-US", {
+      month: "long",
+    }).format(firstDayObj);
+    const M = Y[m]?.length === 37 ? Y[m] : Array(37).fill(0);
 
-        let date = 1;
-        for (let i = 0; i < 37; i++) {
-            const dClone = dayTemplate.content.cloneNode(true);
-            // visibility hidden for unused divs
-            // comment to add hatching instead
-            // const dDiv = dClone.querySelector('.day');
-            const check = dClone.querySelector('.check');
-            const streakmark = dClone.querySelector('.streakmark');
-            if (i < dayOfWeek || noOfDays + dayOfWeek <= i) {
-                check.disabled = true;
-                // dDiv.classList.add('novisibility');
-            }
-            if (i >= dayOfWeek && date <= noOfDays) {
-                streakmark.textContent = date.toString();
-                // putting inside name = yyyy-mm(index)-dd(index)
-                check.name = `${y}-${m}-${i}`;
-                check.checked = !!(M[i] === 1);
-                date++;
-            }
-            mArticle.appendChild(dClone);
-        }
-        ySection.appendChild(mClone);
+    let date = 1;
+    for (let i = 0; i < 37; i++) {
+      const dClone = dayTemplate.content.cloneNode(true);
+      // visibility hidden for unused divs
+      // comment to add hatching instead
+      // const dDiv = dClone.querySelector('.day');
+      const check = dClone.querySelector(".check");
+      const streakmark = dClone.querySelector(".streakmark");
+      if (i < dayOfWeek || noOfDays + dayOfWeek <= i) {
+        check.disabled = true;
+        // dDiv.classList.add('novisibility');
+      }
+      if (i >= dayOfWeek && date <= noOfDays) {
+        streakmark.textContent = date.toString();
+        // putting inside name = yyyy-mm(index)-dd(index)
+        check.name = `${y}-${m}-${i}`;
+        check.checked = !!(M[i] === 1);
+        date++;
+      }
+      mArticle.appendChild(dClone);
     }
-    body.appendChild(yClone);
+    ySection.appendChild(mClone);
+  }
+  body.appendChild(yClone);
 }
